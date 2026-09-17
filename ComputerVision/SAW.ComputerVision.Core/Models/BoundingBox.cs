90bdb4a9fad7d2d5a95c7878659905318ea99c43
viogustian <viognuralamsyah@gmail.com>
2026-09-17 10:33:53 +0700

    feat(backend): Scaffold CV Solution & Build IOU Tracker (#108)

---
+++ ComputerVision/SAW.ComputerVision.Core/Models/BoundingBox.cs
@@ -0,0 +1,37 @@
+namespace SAW.ComputerVision.Core.Models;
+
+public readonly struct BoundingBox
+{
+    public double X { get; }
+    public double Y { get; }
+    public double Width { get; }
+    public double Height { get; }
+
+    public BoundingBox(double x, double y, double width, double height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+    public double Area => Width * Height;
+
+    public double CalculateIou(BoundingBox other)
+    {
+        var xA = Math.Max(X, other.X);
+        var yA = Math.Max(Y, other.Y);
+        var xB = Math.Min(X + Width, other.X + other.Width);
+        var yB = Math.Min(Y + Height, other.Y + other.Height);
+
+        var interArea = Math.Max(0, xB - xA) * Math.Max(0, yB - yA);
+
+        if (interArea == 0) return 0;
+
+        var boxAArea = Area;
+        var boxBArea = other.Area;
+
+        var iou = interArea / (boxAArea + boxBArea - interArea);
+        return iou;
+    }
+}
